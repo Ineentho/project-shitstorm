fa406f8da7e42cc45739d7c099acb22e4651f4c5
Henrik Karlsson <hk.henrik@gmail.com>
2015-06-04 22:26:20 +0200

    Camera following more closely

--- core/src/com/ineentho/shitstorm/util/Utils.java
+++ core/src/com/ineentho/shitstorm/util/Utils.java
@@ -5,6 +5,6 @@ import com.badlogic.gdx.math.Vector2;
 public class Utils {
 	public static Vector2 moveTowards(Vector2 source, Vector2 target) {
 		Vector2 diff = target.sub(source);
-		return source.add(diff.scl(.95f));
+		return source.add(diff.scl(.8f));
 	}
 }
